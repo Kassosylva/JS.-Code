2f1f670abc54813e60a3fbea462ccbb2f80f4976
kassosylva <sylvainkasonga5@gmail.com>
2024-11-28 05:04:10 +0100

    condition: Vérifier si le nombre de taches est zero

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ console.log("Bienvenue dans votre gestionnaire de taches!")
 //j'ai créé une variable de nombre entier et l'afficher
 const nombresEntier = 5;
 console.log(nombresEntier)
+
+//condition: Vérifier si le nombre de taches est zero
+
+let verifierNombreDeTaches = 0;
+
+if (verifierNombreDeTaches === 0){
+    console.log("vrai")
+} else {
+    console.log("faux")
+}
